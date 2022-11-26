509de560b6e1933a0f70c08306303d0ac159e96f
AllwenWeill <1041268906@qq.com>
2022-11-25 18:44:39 -0800

    添加LogError报错信息类

---
+++ include/handlError/LogError.h
@@ -0,0 +1,14 @@
+#include "allinclude.h"
+
+struct errorInformation{ //报错信息结构体
+    int lineNum;
+    string errorStr;
+}
+class LogError{//该类专门负责处理错误信息
+public:
+    vector<string> errorNotes;
+    LogError();
+    ~LogError();    
+    void addnote(string errorStr, int lineNum); //添加报错信息至errorNotes队列
+private:
+};
